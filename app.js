3eb7212060767c031038d25e6cc32419966cb9dc
Stanislav Agaronin <stanislav.agaronin@gmail.com>
2020-09-23 02:19:05 +0300

    Content changed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var http = require('http');
 //create a server object:
 http
   .createServer(function (req, res) {
-    res.write('123'); //write a response to the client
+    res.write('Hello'); //write a response to the client
     res.end(); //end the response
   })
   .listen(8585); //the server object listens on port 8080
